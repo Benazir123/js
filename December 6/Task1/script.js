e447c7dc32a707403e81f0acf312e91123b827f8
Benazir123 <94034815+Benazir123@users.noreply.github.com>
2021-12-06 18:41:12 +0530

    initial commit

---
+++ December 6/Task1/script.js
@@ -0,0 +1,116 @@
+//for redirection when clicking add new button//
+ 
+function addRedirect(){
+    window.location.href = "add.html";
+}
+
+
+
+function editRedirect(rowIndex){
+    window.location.href = "edit.html?rowIndex=" + rowIndex;
+}
+
+
+
+function onDelete(index){
+	contactList.splice(index, 1);
+	localStorage.setItem("contactDetails",JSON.stringify(contactList));
+
+	// contactList = JSON.parse(localStorage.contactDetails);
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // var tbListRow = $(`
+ //     <table class="table table-stripped">
+ //          <thead>
+ //            <tr>
+ //              <th>S.No</th>
+ //              <th>FirstName</th>
+ //              <th>LastName</th>
+ //              <th>E-Mail</th>
+ //              <th>Contact-No</th>
+ //              <th>Active</th>
+ //              <th>Actions</th>
+ //            </tr>
+ //          </thead>
+ //          <tbody>
+ //     `);
+
+
+ //     const contactList = [
+ //       { 
+
+ //        id: document.getElementById("id1").value, 
+ //        firstName: document.getElementById("fname").value, 
+ //        lastName: document.getElementById("lname").value, 
+ //        email: document.getElementById("email").value, 
+ //        phone: document.getElementById("contact").value, 
+ //        active_status: document.getElementById("active_status").value
+ //    }];
+
+ //     for (let index = 0; index < contactList.length; index++) {
+ //       const element = contactList[index];
+       
+
+ //        tbListRow.append(`
+ //        <tr>
+ //              <td>${element.id}</td>
+ //              <td>${element.firstName}</td>
+ //              <td>${element.lastName}</td>
+ //              <td>${element.email}</td>
+ //              <td>${element.phone}</td>
+ //              <td>
+ //                       <input class="form-check-input" type="checkbox" id="flexCheckDefault" ${element.active_status == 1 ? 'checked' : ''}>              
+ //              </td>
+ //              <td>
+ //                <button type="button" id="editBtn" class="btn btn-success">Edit</button>
+ //                <button type="button" id="dltBtn" class="btn btn-danger">Delete</button>
+ //              </td>
+ //        </tr>
+                           
+ //         `);
+
+ //     }
+
+ //     $(".contact-list-details").html(tbListRow);
+
+
